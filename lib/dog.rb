4f0308f40f44397026cd2cb3b3948478ead42116
Cortney Dawkins <cmddawkinscortney@gmail.com>
2020-04-27 15:32:44 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,21 @@
-# Your code goes here!
+#setter
+
+class Dog 
+  def name=(dogs_name) 
+    @this_dogs_name = dogs_name
+end
+
+#getter
+
+def name
+    @this_dogs_name
+  end
+end
+
+#method 
+
+class Dog
+  def bark
+    puts "woof!"
+  end
+end
