dd9ea90270b31e584deff339756bcfc9a5e97416
Joey <joey.alzamora@gmail.com>
2020-04-27 20:54:33 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,14 +2,22 @@
 class Dog 
   
  def name=(dog_name)
-   @dog_name =
-    
+    @this_dogs_name =    #### setter method 
+    dog_name
+  end
+  
+  def name
+    @this_dogs_name     #### getter method 
+  end
 
+  def bark
+    puts "woof!"
+  end 
 
-
-
-
-
+fido = Dog.new
+fido.name = "Fido"
+fido.name
+fido.bark
 
 
 end
