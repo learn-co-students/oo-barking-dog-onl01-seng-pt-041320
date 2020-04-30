5e1df1cdc4f57db43829fdaa442e479c59a05bd3
Aurora Avery <aurora.avery@yahoo.com>
2020-04-30 11:30:11 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,13 +2,17 @@ class Dog
   def name
     @name
   end
+      #has a name
 
   def name=(dog_name)
     @name = dog_name
   end
+      #has a name
 
   def bark
     puts "woof!"
   end
+      #is able to bark
+      
 end
 
